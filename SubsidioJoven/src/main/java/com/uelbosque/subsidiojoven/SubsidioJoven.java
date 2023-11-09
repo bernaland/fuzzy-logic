d5c1626ae1698e9a6ebba05961c49727e1084c7d
Gelen <gruanom@unbosque.edu.co>
2023-11-08 20:29:11 -0500

    Formulario Inicial

---
+++ SubsidioJoven/src/main/java/com/uelbosque/subsidiojoven/SubsidioJoven.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uelbosque.subsidiojoven;
+
+import com.uelbosque.subsidiojoven.formulario.MainFrame;
+
+/**
+ *
+ * @author helen
+ */
+public class SubsidioJoven {
+
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            new MainFrame().setVisible(true);
+         }
+      });
+        
+    }
+}
