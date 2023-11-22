398e051a350ea212f25045592cdb76fec47a3267
bernaland <agbernala@hotmail.com>
2023-11-21 20:23:03 -0500

    Se agrega modelo fuzzy para evaluar las variables (#5)

---
+++ SubsidioJoven/src/main/java/com/uelbosque/subsidiojoven/modelo/FuzzyController.java
@@ -0,0 +1,58 @@
+package com.uelbosque.subsidiojoven.modelo;
+
+import java.io.File;
+import java.util.HashMap;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.LinguisticTerm;
+import net.sourceforge.jFuzzyLogic.rule.Rule;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+public class FuzzyController {
+    private FIS modeloActual;
+
+    public void cargarModelo() {
+        String fileName = "SubsidioJoven.fcl";
+        
+        File modelFile = new File(fileName);
+        FIS fis = FIS.load(modelFile.getAbsolutePath(),true);
+
+        // Error while loading?
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+        // Show 
+        // JFuzzyChart.get().chart(fis);
+        modeloActual = fis;
+    }
+
+    public Probabilidad ejecutarModelo(ParametrosModelo params) {
+        if (params.getEdad() >= 18 && params.getEdad() <= 28) {
+            System.out.println(params);
+            modeloActual.setVariable("ingresos", params.getIngresos());
+            modeloActual.setVariable("puntaje_crediticio", params.getPuntajeCrediticio());
+            modeloActual.setVariable("valor_inmueble", params.getValorInmueble());
+
+            modeloActual.evaluate();
+
+            // FunctionBlock block = modeloActual.getFunctionBlock("subsidioJoven");
+            Variable outVar = modeloActual.getVariable("posibilidad");
+
+            // JFuzzyChart.get().chart(outVar, outVar.getDefuzzifier(), true);
+
+            double outValue = outVar.getValue();
+
+            // Show each rule (and degree of support)
+            // for( Rule r : block.getFuzzyRuleBlock("asignacion").getRules() )
+            //     System.out.println(r);
+
+            return outValue >= 0.6 ? Probabilidad.Alta 
+                : (outValue >= 0.4 ? Probabilidad.Media : Probabilidad.Baja);
+        }
+        return Probabilidad.Baja;
+    }
+}
